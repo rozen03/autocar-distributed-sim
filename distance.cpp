180ca0e5a54ced12258cc72480f621e8e5492156
rozen03 <urielrozenberg@hotmail.com>
2018-08-07 18:45:09 -0300

    prettyfy code

--- distance.cpp
+++ distance.cpp
@@ -15,26 +15,26 @@ double colide(Line line1, Line line2) {
 		double y12 = line2.y1;
 		double z11 = line1.z1;
 		double z12 = line2.z1;
-		double dir1_x=x11-x01;
-		double dir1_y=y11-y01;
-		double dir1_z=z11-z01;
-		double dir2_x=x12-x02;
-		double dir2_y=y12-y02;
-		double dir2_z=z12-z02;
-		double cross_x=dir1_x*dir2_x;
-		double cross_y=dir1_y*dir2_y;
-		double cross_z=dir1_z*dir2_z;
+		double dir1_x = x11 - x01;
+		double dir1_y = y11 - y01;
+		double dir1_z = z11 - z01;
+		double dir2_x = x12 - x02;
+		double dir2_y = y12 - y02;
+		double dir2_z = z12 - z02;
+		double cross_x = dir1_x * dir2_x;
+		double cross_y = dir1_y * dir2_y;
+		double cross_z = dir1_z * dir2_z;
 		//if we get cero, the two directions are parallel, somehow...
-		double crossNorm= norm(cross_x,cross_y,cross_z);
-		if(crossNorm==0.0){return -1.0;}
+		double crossNorm = norm(cross_x,cross_y,cross_z);
+		if( crossNorm == 0.0 ){ return -1.0; }
 		//normalize product
 		cross_x /= crossNorm;
 		cross_y /= crossNorm;
 		cross_z /= crossNorm;
-		double diff_x= x01-x02;
-		double diff_y= y01-y02;
-		double diff_z= z01-z02;
-		double prod= cross_x*diff_x + cross_y*diff_y+cross_z*diff_z;
+		double diff_x = x01 - x02;
+		double diff_y = y01 - y02;
+		double diff_z = z01 - z02;
+		double prod = cross_x * diff_x + cross_y * diff_y + cross_z * diff_z;
 		double dist = abs(prod);
 		return dist;
 }
